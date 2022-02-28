5d0c4ebdfde73f801f4354950d0faf06ef779902
Ilya <ilyha575@inbox.ru>
2022-02-28 18:58:04 +1000

    rest api for product

---
+++ src/main/java/com/geekbrains/gbrestdemo/entities/Product.java
@@ -0,0 +1,42 @@
+package com.geekbrains.gbrestdemo.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
